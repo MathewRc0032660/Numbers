ecd56b76ea0284f7731cb0b39be103da25649fa6
Mathew Holland <rc0032660@student.riversidecollege.ac.uk>
2018-12-03 15:24:02 +0000

    numbers

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -14,7 +14,25 @@ namespace Numbers
     {
         public Form1()
         {
-            InitializeComponent();
+         InitializeComponent();
+         double d1 = 5e6;
+         richTextBox1.AppendText(d1.ToString() + "\n");
+         richTextBox1.AppendText(d1.ToString("N") + "\n");
+         richTextBox1.AppendText(d1.ToString("N3") + "\n");
+         richTextBox1.AppendText(d1.ToString("N0") + "\n");
+         richTextBox1.AppendText(d1.ToString("0") + "\n");
+         richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+         richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
+         richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+
+
+            double pi = 3.141592653589793;
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N9") + "\n");
+
+            double c = 299792458
+
         }
     }
 }
